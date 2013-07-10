45a0fe4834816c91a0ca1242b92de08f1e131ae1
stormgrab <stormgrab@gmail.com>
2013-07-10 23:05:24 +0530

    Added feature to edit user in backend

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < Admin::BaseController
+	before_filter :find_user, :only => [:show,:edit,:update,:delete]
+
 	def index
 		@users = User.all(:order => "email")
 	end
@@ -17,5 +19,37 @@ class Admin::UsersController < Admin::BaseController
 			flash[:alert] = "User has not been created"
 			render :action => "new"
 		end
+	end
+
+	def show
+		
+	end
+
+	def edit
+
+	end
+
+	def update
+		admin = params[:user].delete(:admin)
+		@user.admin = admin == "1"
+		
+		if params[:user][:password].blank?
+			params[:user].delete(:password)
+			params[:user].delete(:password_confirmation)
+		end
+
+		if @user.update_attributes(params[:user])
+			flash[:notice] = "User has been updated."
+			redirect_to admin_users_path
+		else
+			flash[:alert] = "User has not been updated."
+			render :action => "edit"
+		end
+	end
+
+
+	private
+	def find_user
+		@user = User.find(params[:id])
 	end
 end
